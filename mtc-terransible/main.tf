dac63c35b31eb47c6ccd74643eb5376b2bdc4d5e
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2025-03-02 22:07:24 +0000

    #11 initialised the project (#12)

---
+++ mtc-terransible/main.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc" "mtc_vpc" {
+    cidr_block = "10.2.0.0/16"
+    enable_dns_hostnames = true
+    enable_dns_support = true
+
+    tags = {
+        name = "mtc_vpc"
+    }
+  
+}
