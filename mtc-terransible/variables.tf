8a08bdb34fd2aa2b592d804a71541ab8280746e5
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2025-03-03 00:06:33 +0000

    #15 converted to goithub codespace

--- mtc-terransible/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "aws_role_arn" {
-  description = "The AWS role to assume"
-  type        = string
-  default     = "arn:aws:iam::182399680009:role/terransible-DevOpsTerraformRole-dFpTShn4FLF6"
-}
